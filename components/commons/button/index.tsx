6eaaf64e114a80ac1cf2294e8c3e19275c5ecfb5
Abba Sali <abbasaliaboubakar@gmail.com>
2023-08-19 19:30:28 +0100

    update btns

--- components/commons/button/index.tsx
+++ components/commons/button/index.tsx
@@ -1,12 +1,12 @@
 import {FaChevronRight} from "react-icons/fa";
-interface ButtonProps extends React.AllHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     variant?: string;
 }
 
 const MyButton = ({children, className, variant, ...props}: ButtonProps) => {
     if (variant === "BORDER")
-        return <button className={`border rounded-md text-sm text-gray-300 border-gray-600 px-4 py-2 ${className}`} {...props}>{children}</button>
-    return <button className={`bg-white text-[#0E372D] text-sm rounded-md py-2 px-4 flex justify-center items-center ${className}`} {...props}>{children}<span className={'ml-2'}><FaChevronRight size={10} className={'bg-white'} /></span></button>
+        return <button type="button" className={`border rounded-md text-sm text-gray-300 border-gray-600 px-4 py-2 ${className}`} {...props}>{children}</button>
+    return <button type="button" className={`bg-white text-[#0E372D] text-sm rounded-md py-2 px-4 flex justify-center items-center ${className}`} {...props}>{children}<span className={'ml-2'}><FaChevronRight size={10} className={'bg-white'} /></span></button>
 }
 
 export {MyButton};
